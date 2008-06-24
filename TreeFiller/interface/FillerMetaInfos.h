d34ed0b078cdda89759cc0808ec54bccf25caa05
loizides <>
2008-06-24 14:24:55 +0000

    Have RunInfo stored.

--- TreeFiller/interface/FillerMetaInfos.h
+++ TreeFiller/interface/FillerMetaInfos.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: FillerMetaInfos.h,v 1.1 2008/06/19 16:53:43 loizides Exp $
+// $Id: FillerMetaInfos.h,v 1.2 2008/06/20 17:52:57 loizides Exp $
 //
 // FillerMetaInfos
 //
@@ -11,6 +11,7 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "MitAna/DataTree/interface/EventHeader.h"
+#include "MitAna/DataTree/interface/LAHeader.h"
 #include "MitAna/DataTree/interface/RunInfo.h"
 #include "MitProd/TreeFiller/interface/BaseFiller.h"
 
@@ -27,13 +28,17 @@ namespace mithep
       void FillRunInfo(const edm::Event&, const edm::EventSetup&);
   
     private:
-      std::string              evtName_;
-      std::string              runName_;
-      EventHeader             *eventHeader_;
-      RunInfo                 *runInfo_;
-      TTree                   *runTree_;
-      Int_t                    runEntries_;
-      std::map<UInt_t,Int_t>   runmap_;
+      TreeWriter              *tws_;          //tree writer (not owned)
+      std::string              evtName_;      //event branch name
+      std::string              runName_;      //run info branch name
+      EventHeader             *eventHeader_;  //event header
+      LAHeader                *evtLAHeader_;  //look-ahead event header
+      RunInfo                 *runInfo_;      //run info block
+      TTree                   *runTree_;      //run info tree
+      TTree                   *laTree_;       //look ahead tree
+      Int_t                    runEntries_;   //number of run info entries
+      UShort_t                 fileNum_;      //file number of current file
+      std::map<UInt_t,Int_t>   runmap_;       //map between run number and entry number
   };
 }
 #endif
--- TreeFiller/src/FillerMetaInfos.cc
+++ TreeFiller/src/FillerMetaInfos.cc
@@ -1,4 +1,4 @@
-// $Id: FillerMetaInfos.cc,v 1.1 2008/06/19 16:53:43 loizides Exp $
+// $Id: FillerMetaInfos.cc,v 1.2 2008/06/20 17:52:57 loizides Exp $
 
 #include "MitProd/TreeFiller/interface/FillerMetaInfos.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -12,49 +12,82 @@ using namespace mithep;
 //-------------------------------------------------------------------------------------------------
 FillerMetaInfos::FillerMetaInfos(const ParameterSet &cfg) : 
   BaseFiller(cfg,"MetaInfos"),
+  tws_(0),
   evtName_(Conf().getUntrackedParameter<string>("evtName",Names::gkEvtHeaderBrn)),
   runName_(Conf().getUntrackedParameter<string>("runName",Names::gkRunInfoBrn)),
   eventHeader_(new EventHeader()),
+  evtLAHeader_(new LAHeader()),
   runInfo_(new RunInfo()),
   runTree_(0),
-  runEntries_(0)
+  laTree_(0),
+  runEntries_(0),
+  fileNum_(0)
 {
+  // Constructor.
 }
 
 //-------------------------------------------------------------------------------------------------
 FillerMetaInfos::~FillerMetaInfos()
 {
+  // Destructor.
+
   delete eventHeader_;
-  eventHeader_=0;
   delete runInfo_;
-  runInfo_=0;
-  runTree_=0;
+  eventHeader_ = 0;
+  runInfo_     = 0;
+  runTree_     = 0;
+  laTree_      = 0;
 }
 
 //-------------------------------------------------------------------------------------------------
 void FillerMetaInfos::BookDataBlock(TreeWriter &tws)
 {
+  // Create run info tre and book our branches.
+
+  // add branches to main tree
   tws.AddBranch(evtName_.c_str(),"mithep::EventHeader",&eventHeader_);
-  tws.AddBranchToTree(Names::gkRunTreeName, runName_.c_str(),"mithep::RunInfo",&runInfo_);
+
+  // add branches to run info tree
+  tws.AddBranchToTree(Names::gkRunTreeName,runName_.c_str(),"mithep::RunInfo",&runInfo_);
   tws.SetAutoFill(Names::gkRunTreeName,0);
   runTree_=tws.GetTree(Names::gkRunTreeName);
+
+  // add branches to lookahead tree
+  tws.AddBranchToTree(Names::gkLATreeName,Names::gkLAHeaderBrn,"mithep::LAHeader",&evtLAHeader_);
+  tws.SetAutoFill(Names::gkLATreeName,0);
+  laTree_=tws.GetTree(Names::gkLATreeName);
+
+  // store pointer to tree writer 
+  tws_ = &tws;
 }
 
 //-------------------------------------------------------------------------------------------------
 void FillerMetaInfos::FillDataBlock(const edm::Event      &event, 
                                     const edm::EventSetup &setup)
 {
+  // Fill our data structures.
+
+  // clear map if a new file was opened
+  if (tws_->GetFileNumber()!=fileNum_) {
+    runmap_.clear();
+    fileNum_ = tws_->GetFileNumber();
+    runEntries_ = 0;
+  }
+
+  UInt_t runnum = event.id().run();
+
+  // store look ahead information
+  if (runEntries_>0) {
+    evtLAHeader_->SetRunNum(runnum);
+    laTree_->Fill();
+  }
+
   // fill event header
   eventHeader_->SetEvtNum(event.id().event());
   eventHeader_->SetLumiSec(event.luminosityBlock());
-  UInt_t runnum = event.id().run();
   eventHeader_->SetRunNum(runnum);
 
-  // should check here if file was closed and
-  // reset map if needed. This will all come, 
-  // eventually split over methods or even in 
-  // separate filler.
-
+  // look-up if entry is in map
   map<UInt_t,Int_t>::iterator riter = runmap_.find(runnum);
   if (riter != runmap_.end()) {
     Int_t runentry = riter->second;
@@ -69,5 +102,6 @@ void FillerMetaInfos::FillDataBlock(const edm::Event      &event,
 
   runInfo_->SetRunNum(runnum);
   runTree_->Fill();
+
   ++runEntries_;
 }
