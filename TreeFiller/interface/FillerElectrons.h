1415cf42686ac92f21c7210b9b0ad2a66cc7a797
sixie <>
2008-08-18 11:34:02 +0000

    updated to 2_1_X

--- TreeFiller/interface/FillerElectrons.h
+++ TreeFiller/interface/FillerElectrons.h
@@ -1,11 +1,11 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: FillerElectrons.h,v 1.4 2008/07/31 12:34:04 loizides Exp $
+// $Id: FillerElectrons.h,v 1.5 2008/08/08 11:21:08 sixie Exp $
 //
 // FillerElectrons
 //
 // Imlementation of a filler to fill EDM electrons into our mithep::Electron data structure.
 //
-// Authors: J.Bendavid
+// Authors: J.Bendavid, S.Xie
 //--------------------------------------------------------------------------------------------------
 
 #ifndef TREEFILLER_FILLERELECTRONS_H
@@ -13,7 +13,7 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DataFormats/GsfTrackReco/interface/GsfTrackFwd.h"
-#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectronFwd.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
 #include "MitAna/DataUtil/interface/TreeWriter.h"
 #include "MitAna/DataTree/interface/Collections.h"
 #include "MitProd/TreeFiller/interface/BaseFiller.h"
@@ -25,9 +25,6 @@ namespace mithep
   {  
     public:
 
-/*       FillerElectrons(const edm::ParameterSet &cfg, bool active=1, */
-/*                       const GsfTrackMap* gsfTrackMap=0, const TrackMap* trackerTrackMap=0, const SuperClusterMap* superClusterMap=0); */
-
       FillerElectrons(const edm::ParameterSet &cfg, bool active=1);
       ~FillerElectrons();
 
@@ -36,15 +33,31 @@ namespace mithep
   
     private:
 
-      std::string                    edmName_;               //edm name of electrons collection
-      std::string                    mitName_;               //name of Electrons in OAK
-      std::string                    gsfTrackMapName_;       //name of imported map wrt gsf tracks
-      std::string                    trackerTrackMapName_;   //name of imported map wrt to trk tracks 
-      std::string                    superClusterMapName_;   //name of imported map wrt to super clusters
-      mithep::ElectronArr            *electrons_;            //array of Electrons
-      const mithep::GsfTrackMap      *gsfTrackMap_;          //map wrt gsf tracks
-      const mithep::TrackMap         *trackerTrackMap_;      //map wrt tracker tracks
-      const mithep::SuperClusterMap  *superClusterMap_;      //map wrt super clusters
+      std::string                    edmName_;                   //edm name of electrons collection
+      std::string                    mitName_;                   //name of Electrons in OAK
+      std::string                    gsfTrackMapName_;           //name of imported map wrt gsf tracks
+      std::string                    trackerTrackMapName_;       //name of imported map wrt 
+                                                                 //to trk tracks 
+      std::string                    barrelEcalRecHitName_;      //name of barrel ecal rechits 
+      std::string                    endcapEcalRecHitName_;      //name of endcap ecal rechits
+      std::string                    barrelSuperClusterName_;    //name of barrel super clusters 
+      std::string                    endcapSuperClusterName_;    //name of endcap super clusters
+      std::string                    barrelBasicClusterName_;    //name of barrel basic clusters 
+      std::string                    endcapBasicClusterName_;    //name of endcap basic clusters
+      std::string                    barrelSuperClusterMapName_; //name of imported map wrt to 
+                                                                 //barrel super clusters
+      std::string                    endcapSuperClusterMapName_; //name of imported map wrt to 
+                                                                 //endcap super cluster
+      std::string                    eIDCutBasedName_;           //name of cut based eID algo
+      std::string                    eIDCutBasedClassesName_;    //name of cut based classes eID algo
+      std::string                    eIDLikelihoodName_;         //name of likelihood eID algo
+      std::string                    eIDNeuralNetName_;          //name of neural net eID algo
+
+      mithep::ElectronArr            *electrons_;                //array of Electrons
+      const mithep::GsfTrackMap      *gsfTrackMap_;              //map wrt gsf tracks
+      const mithep::TrackMap         *trackerTrackMap_;          //map wrt tracker tracks
+      const mithep::SuperClusterMap  *barrelSuperClusterMap_;    //map wrt barrel super clusters
+      const mithep::SuperClusterMap  *endcapSuperClusterMap_;    //map wrt endcap super clusters 
 
   };
 }
--- TreeFiller/src/FillerElectrons.cc
+++ TreeFiller/src/FillerElectrons.cc
@@ -1,4 +1,4 @@
-// $Id: FillerElectrons.cc,v 1.10 2008/07/31 12:34:04 loizides Exp $
+// $Id: FillerElectrons.cc,v 1.11 2008/08/08 11:21:08 sixie Exp $
 
 #include "MitProd/TreeFiller/interface/FillerElectrons.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
@@ -14,6 +14,9 @@
 #include "RecoEgamma/EgammaIsolationAlgos/interface/EgammaEcalIsolation.h"
 #include "MitAna/DataTree/interface/Track.h"
 #include "MitAna/DataTree/interface/Names.h"
+#include "RecoEcal/EgammaCoreTools/interface/EcalClusterLazyTools.h"
+#include "AnalysisDataFormats/Egamma/interface/ElectronID.h"
+#include "AnalysisDataFormats/Egamma/interface/ElectronIDAssociation.h"
 
 using namespace std;
 using namespace edm;
@@ -26,12 +29,25 @@ FillerElectrons::FillerElectrons(const edm::ParameterSet &cfg, bool active) :
   mitName_(Conf().getUntrackedParameter<string>("mitName",Names::gkElectronBrn)),
   gsfTrackMapName_(Conf().getUntrackedParameter<string>("gsfTrackMapName","")),
   trackerTrackMapName_(Conf().getUntrackedParameter<string>("trackerTrackMapName","")),
-  superClusterMapName_(Conf().getUntrackedParameter<string>("superClusterMapName", "")),
-  electrons_(new mithep::ElectronArr(16)),
+  barrelEcalRecHitName_(Conf().getUntrackedParameter<string>("barrelEcalRecHitName","")),
+  endcapEcalRecHitName_(Conf().getUntrackedParameter<string>("endcapEcalRecHitName","")),
+  barrelSuperClusterName_(Conf().getUntrackedParameter<string>("barrelSuperClusterName", "")),
+  endcapSuperClusterName_(Conf().getUntrackedParameter<string>("endcapSuperClusterName", "")),
+  barrelBasicClusterName_(Conf().getUntrackedParameter<string>("barrelBasicClusterName", "")),
+  endcapBasicClusterName_(Conf().getUntrackedParameter<string>("endcapBasicClusterName", "")),
+  barrelSuperClusterMapName_(Conf().getUntrackedParameter<string>("barrelSuperClusterMapName","")),
+  endcapSuperClusterMapName_(Conf().getUntrackedParameter<string>("endcapSuperClusterMapName","")),
+  eIDCutBasedName_(Conf().getUntrackedParameter<string>("eIDCutBasedName","eidCutBased")),         
+  eIDCutBasedClassesName_(Conf().getUntrackedParameter<string>("eIDCutBasedClassesName",
+                                                               "eidCutBasedClasses'")),
+  eIDLikelihoodName_(Conf().getUntrackedParameter<string>("eIDLikelihood","eidLikelihood")),     
+  eIDNeuralNetName_(Conf().getUntrackedParameter<string>("eIDNeuralNet","eidNeuralNet")),
 
+  electrons_(new mithep::ElectronArr(16)),
   gsfTrackMap_(0),
   trackerTrackMap_(0),
-  superClusterMap_(0)
+  barrelSuperClusterMap_(0),
+  endcapSuperClusterMap_(0)
 {
   // Constructor.
 }
@@ -55,9 +71,10 @@ void FillerElectrons::BookDataBlock(TreeWriter &tws)
     gsfTrackMap_ = OS()->get<GsfTrackMap>(gsfTrackMapName_.c_str());
   if (!trackerTrackMapName_.empty()) 
     trackerTrackMap_ = OS()->get<TrackMap>(trackerTrackMapName_.c_str());  
-  if (!superClusterMapName_.empty())
-    superClusterMap_ = OS()->get<SuperClusterMap>(superClusterMapName_.c_str());
-
+  if (!barrelSuperClusterMapName_.empty())
+    barrelSuperClusterMap_ = OS()->get<SuperClusterMap>(barrelSuperClusterMapName_.c_str());
+  if (!endcapSuperClusterMapName_.empty())
+    endcapSuperClusterMap_ = OS()->get<SuperClusterMap>(endcapSuperClusterMapName_.c_str());
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -70,34 +87,24 @@ void FillerElectrons::FillDataBlock(const edm::Event &event, const edm::EventSet
   Handle<reco::GsfElectronCollection> hElectronProduct;
   GetProduct(edmName_, hElectronProduct, event);
   
-  const reco::GsfElectronCollection inElectrons = *(hElectronProduct.product());
-  
-  
-  //get association of clusters to their shapes for EcalBarrel
-  edm::Handle<reco::BasicClusterShapeAssociationCollection> barrelClShpHandle ;
-  try { 
-    event.getByLabel ("hybridSuperClusters","hybridShapeAssoc", barrelClShpHandle);
-  }
-  catch ( cms::Exception& ex ) {
-    printf("Can't get ECAL barrel Cluster Shape Collection\n");
-  }
-  const reco::BasicClusterShapeAssociationCollection& barrelClShpMap = *barrelClShpHandle ;
-  
-  //get the association of the clusters to their shapes for EcalEndcap
-  edm::Handle<reco::BasicClusterShapeAssociationCollection> endcapClShpHandle ;
-  try {
-    event.getByLabel ("islandBasicClusters","islandEndcapShapeAssoc", endcapClShpHandle);
-  }
-  catch ( cms::Exception& ex ) {
-    printf("Can't get ECAL endcap Cluster Shape Collection\n");
-  }
-  const reco::BasicClusterShapeAssociationCollection& endcapClShpMap = *endcapClShpHandle ;
+  //Get Official Electron ID Results
+  //cout << "try electron ID \n";
+  Handle<reco::GsfElectronRefVector> hCutBasedElectronIDProduct;
+  GetProduct(eIDCutBasedName_ , hCutBasedElectronIDProduct, event);
+  Handle<reco::GsfElectronRefVector> hCutBasedClassesElectronIDProduct;
+  GetProduct(eIDCutBasedClassesName_ , hCutBasedClassesElectronIDProduct, event);
+  Handle<reco::GsfElectronRefVector> hLikelihoodElectronIDProduct;
+  GetProduct(eIDLikelihoodName_ , hLikelihoodElectronIDProduct, event);
+  Handle<reco::GsfElectronRefVector> hNeuralNetElectronIDProduct;
+  GetProduct(eIDNeuralNetName_ , hNeuralNetElectronIDProduct, event);
 
+  const reco::GsfElectronCollection inElectrons = *(hElectronProduct.product());
   //loop over electrons
+  unsigned int i = 0;
   for (reco::GsfElectronCollection::const_iterator iM = inElectrons.begin(); 
        iM != inElectrons.end(); ++iM) {
     mithep::Electron *outElectron = electrons_->AddNew();
-    
+         
     outElectron->SetESuperClusterOverP( iM->eSuperClusterOverP() ) ;
     outElectron->SetESeedClusterOverPout( iM->eSeedClusterOverPout() ) ;
     outElectron->SetDeltaEtaSuperClusterTrackAtVtx( iM->deltaEtaSuperClusterTrackAtVtx() ) ;
@@ -109,61 +116,65 @@ void FillerElectrons::FillDataBlock(const edm::Event &event, const edm::EventSet
     outElectron->SetIsMomentumCorrected( iM->isMomentumCorrected() ) ;
     outElectron->SetNumberOfClusters( iM->numberOfClusters() ) ;
     outElectron->SetClassification( iM->classification() ) ;
-    outElectron->SetSuperClusterPx( iM->px() );
-    outElectron->SetSuperClusterPy( iM->py() );
-    outElectron->SetSuperClusterPz( iM->pz() );
-    outElectron->SetSuperClusterE( iM->energy() );
 
     //initialize
     outElectron->SetE33( -999.0 ) ;
     outElectron->SetE55( -999.0 ) ;
     outElectron->SetCovEtaEta( -999.0 ) ;
     outElectron->SetCovEtaPhi( -999.0 ) ;
-    outElectron->SetCovPhiPhi( -999.0 ) ;
-    bool hasBarrel=true ;
-    bool hasEndcap=true ;    
-    
-    reco::SuperClusterRef sclusRef = iM->get<reco::SuperClusterRef> () ;            
-    reco::BasicClusterShapeAssociationCollection::const_iterator seedShpItr ;
-    //find the cluster shape object associated with the electron supercluster
-    seedShpItr = barrelClShpMap.find (sclusRef->seed()) ;
-    if ( seedShpItr==barrelClShpMap.end())  {
-      hasBarrel=false ;
-      seedShpItr=endcapClShpMap.find (sclusRef->seed()) ;
-      if ( seedShpItr==endcapClShpMap.end() ) hasEndcap=false;
-    }
-    if (hasBarrel || hasEndcap) {
-      //fill shape variables
-      const reco::ClusterShapeRef& sClShape = seedShpItr->val;
-      outElectron->SetE33		      (sClShape->e3x3()     );
-      outElectron->SetE55		      (sClShape->e5x5()     );
-      outElectron->SetCovEtaEta		      (sClShape->covEtaEta());
-      outElectron->SetCovEtaPhi		      (sClShape->covEtaPhi());
-      outElectron->SetCovPhiPhi		      (sClShape->covPhiPhi());
-    }
-    
+    outElectron->SetCovPhiPhi( -999.0 ) ;        
+   
+    EcalClusterLazyTools lazyTools( event, setup, edm::InputTag(barrelEcalRecHitName_), 
+                                    edm::InputTag(endcapEcalRecHitName_) );        
+
+    //Shower shape variables
+    outElectron->SetE33	 (lazyTools.e3x3( *(iM->superCluster()->seed()) ));
+    outElectron->SetE55	 (lazyTools.e5x5( *(iM->superCluster()->seed()) ));
+    std::vector<float> vCov = lazyTools.covariances( *(iM->superCluster()->seed()) );
+    outElectron->SetCovEtaEta(vCov[0]);
+    outElectron->SetCovEtaPhi(vCov[1]);
+    outElectron->SetCovPhiPhi(vCov[2]);    
+
     //Compute Isolations
     //Get the barrel basicClusters 
-    edm::Handle<reco::BasicClusterCollection> barrelBasicClusterHandle;
-    event.getByLabel("hybridSuperClusters", barrelBasicClusterHandle);     
+    edm::Handle<reco::BasicClusterCollection> barrelBasicClusterHandle;    
+    GetProduct(barrelBasicClusterName_, barrelBasicClusterHandle, event);
     const reco::BasicClusterCollection* barrelBasicClusters = barrelBasicClusterHandle.product();
 
     //Get the  endcap basicClusters
     edm::Handle<reco::BasicClusterCollection> endcapBasicClusterHandle;
-    event.getByLabel("islandBasicClusters", "islandEndcapBasicClusters", endcapBasicClusterHandle);
+    GetProduct(endcapBasicClusterName_, endcapBasicClusterHandle, event);
     const reco::BasicClusterCollection* endcapBasicClusters = endcapBasicClusterHandle.product();
     
-    //Get the superClusters
-    edm::Handle<reco::SuperClusterCollection> superClusterHandle;
-    event.getByLabel("hybridSuperClusters", superClusterHandle);
-    const reco::SuperClusterCollection* superClusters = superClusterHandle.product();
-    
+    //Get the barrel superClusters
+    edm::Handle<reco::SuperClusterCollection> barrelSuperClusterHandle;
+    GetProduct(barrelSuperClusterName_, barrelSuperClusterHandle, event);
+    const reco::SuperClusterCollection* barrelSuperClusters = barrelSuperClusterHandle.product();  
+
+    //Get the endcap superClusters
+    edm::Handle<reco::SuperClusterCollection> endcapSuperClusterHandle;
+    GetProduct(endcapSuperClusterName_, endcapSuperClusterHandle, event);
+    const reco::SuperClusterCollection* endcapSuperClusters = endcapSuperClusterHandle.product();
+
+    //find out whether this electron super cluster is in the barrel or endcap
+    bool isBarrel=false ;    
+    if(barrelSuperClusterMap_->HasMit(iM->superCluster()))
+    //if (iM->superCluster()->eta() < 1.49) //If above method doesn't work, just use eta
+      isBarrel = true;
+   
     //compute ECAL isolation
     double extRadius = 0.3;
     double etLow = 0.0;
-    EgammaEcalIsolation myEcalIsolation(extRadius,etLow,barrelBasicClusters,superClusters);
-    double ecalIsoValue = myEcalIsolation.getEcalEtSum(&(*iM));
+    EgammaEcalIsolation *myEcalIsolation = 
+      new EgammaEcalIsolation(extRadius,etLow,barrelBasicClusters,barrelSuperClusters);
 
+    if (!isBarrel) {
+      delete myEcalIsolation;
+      myEcalIsolation = 
+        new EgammaEcalIsolation(extRadius,etLow,endcapBasicClusters,endcapSuperClusters);
+    }
+    double ecalIsoValue = myEcalIsolation->getEcalEtSum(&(*iM));
+    
     //Compute Track Isolation        
     edm::Handle<reco::TrackCollection> tracks;
     event.getByLabel("generalTracks",tracks);
@@ -184,8 +195,13 @@ void FillerElectrons::FillDataBlock(const edm::Event &event, const edm::EventSet
       outElectron->SetGsfTrk(gsfTrackMap_->GetMit(iM->gsfTrack()));
     if (trackerTrackMap_ && iM->track().isNonnull()) 
       outElectron->SetTrackerTrk(trackerTrackMap_->GetMit(iM->track()));
-    if (superClusterMap_ && iM->superCluster().isNonnull())
-      outElectron->SetSuperCluster(superClusterMap_->GetMit(iM->superCluster()));
-  }
+    if (barrelSuperClusterMap_ && endcapSuperClusterMap_ && iM->superCluster().isNonnull())
+      if(isBarrel) {
+        outElectron->SetSuperCluster(barrelSuperClusterMap_->GetMit(iM->superCluster()));        
+      } else {
+        outElectron->SetSuperCluster(endcapSuperClusterMap_->GetMit(iM->superCluster()));
+      }
+  
+  }  
   electrons_->Trim();
 }
