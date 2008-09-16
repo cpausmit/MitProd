d2a431ebbc476e0cc420d4bbc36e5a15e4a1adb9
sixie <>
2008-09-16 18:20:46 +0000

    add names of jet to vertex association and jet corrections

--- TreeFiller/interface/FillerCaloJets.h
+++ TreeFiller/interface/FillerCaloJets.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: FillerCaloJets.h,v 1.6 2008/09/09 12:51:38 sixie Exp $
+// $Id: FillerCaloJets.h,v 1.7 2008/09/10 03:30:22 loizides Exp $
 //
 // FillerCaloJets
 //
@@ -32,8 +32,14 @@ namespace mithep
     private:
       Bool_t                flavorMatchingActive_;            //=true if flavor matching is done  
       Bool_t                bTaggingActive_;                  //=true if bTagging info is filled
+      Bool_t                jetToVertexActive_;               //=true if jet to vertex info is done
+      Bool_t                jetCorrectionsActive_;            //=true if jet corrections are done
       std::string           edmName_;                         //edm name of jets collection
       std::string           mitName_;                         //name of Jets in OAK
+      std::string           jetToVertexAlphaName_;            //edm name of jet to vertex alpha coll
+      std::string           jetToVertexBetaName_;             //edm name of jet to vertex beta coll
+      std::string           L2JetCorrectorName_;              //label of the L2JetCorrection service
+      std::string           L3JetCorrectorName_;              //label of the L3JetCorrection service
       std::string           flavorMatchingByReferenceName_;   //edm name of jets collection
       std::string           flavorMatchingDefinition_;        //name of Jets in OAK
       std::string           jetProbabilityBJetTagsName_;      //names for various bJet Tagging
