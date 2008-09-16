e833fa535c1ed66f7ef3079ebe75e239cdf3dba1
sixie <>
2008-09-16 18:23:06 +0000

    Add flavor matching information for gen jets as well

--- TreeFiller/python/JetsMCFlavourMatching_cfi.py
+++ TreeFiller/python/JetsMCFlavourMatching_cfi.py
@@ -104,10 +104,24 @@ KT6byValAlgo = cms.EDFilter("JetFlavourIdentifier",
     physicsDefinition = cms.bool(False)
 )
 
+#Flavour by Reference
+GenIC5byRef = cms.EDFilter("JetPartonMatcher",
+    jets = cms.InputTag("iterativeCone5GenJets"),
+    coneSizeToAssociate = cms.double(0.3),
+    partons = cms.InputTag("myPartons")
+)
+
+#Flavour by Reference
+GenSC5byRef = cms.EDFilter("JetPartonMatcher",
+    jets = cms.InputTag("sisCone5GenJets"),
+    coneSizeToAssociate = cms.double(0.3),
+    partons = cms.InputTag("myPartons")
+)
 
 iterativeCone5Flavour = cms.Sequence(IC5byRef*IC5byValPhys*IC5byValAlgo)
 sisCone5Flavour = cms.Sequence(SC5byRef*SC5byValPhys*SC5byValAlgo)
 sisCone7Flavour = cms.Sequence(SC7byRef*SC7byValPhys*SC7byValAlgo)
 KT4Flavour = cms.Sequence(KT4byRef*KT4byValPhys*KT4byValAlgo)
 KT6Flavour = cms.Sequence(KT6byRef*KT6byValPhys*KT6byValAlgo)
-caloJetMCFlavour = cms.Sequence(myPartons*iterativeCone5Flavour+sisCone5Flavour+sisCone7Flavour+KT4Flavour+KT6Flavour)
+GenJetFlavour = cms.Sequence(GenIC5byRef*GenSC5byRef)
+caloJetMCFlavour = cms.Sequence(myPartons*iterativeCone5Flavour+sisCone5Flavour+sisCone7Flavour+KT4Flavour+KT6Flavour+GenJetFlavour)
