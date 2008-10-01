d1c3859050be903df81ac71da4301c545399bcf8
bendavid <>
2008-10-01 00:23:35 +0000

    Removed non-working seqeuences

--- TreeFiller/python/MitPostRecoGenerator_cff.py
+++ TreeFiller/python/MitPostRecoGenerator_cff.py
@@ -1,4 +1,4 @@
-# $Id:$
+# $Id: MitPostRecoGenerator_cff.py,v 1.3 2008/09/28 02:27:52 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -9,11 +9,11 @@ from SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi import *
 from SimTracker.TrackAssociation.TrackAssociatorByHits_cfi import *
 TrackAssociatorByHits.Cut_RecoToSim = 0.5
 from SimTracker.TrackAssociation.trackMCMatch_cfi import *
-from SimTracker.TrackAssociation.standAloneMuonsMCMatch_cfi import *
-from SimTracker.TrackAssociation.globalMuonsMCMatch_cfi import *
-from SimTracker.TrackAssociation.allTrackMCMatch_cfi import *
+#from SimTracker.TrackAssociation.standAloneMuonsMCMatch_cfi import *
+#from SimTracker.TrackAssociation.globalMuonsMCMatch_cfi import *
+#from SimTracker.TrackAssociation.allTrackMCMatch_cfi import *
 from SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cff import *
-trackMCMatchSequence = cms.Sequence(trackMCMatch*standAloneMuonsMCMatch*globalMuonsMCMatch*allTrackMCMatch*trackingParticleRecoTrackAsssociation*assoc2GsfTracks*assocOutInConversionTracks*assocInOutConversionTracks)
+trackMCMatchSequence = cms.Sequence(trackMCMatch*trackingParticleRecoTrackAsssociation*assoc2GsfTracks*assocOutInConversionTracks*assocInOutConversionTracks)
 
 # define post-reco generator sequence
 mit_postreco_generator = cms.Sequence(trackMCMatchSequence)
