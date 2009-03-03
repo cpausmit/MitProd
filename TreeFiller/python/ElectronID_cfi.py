d1d9d062fce8a6054b4647f0e49e52936f6e53d3
bendavid <>
2009-03-03 18:52:14 +0000

    Run only eid sequences which are needed in 2_2_x

--- TreeFiller/python/ElectronID_cfi.py
+++ TreeFiller/python/ElectronID_cfi.py
@@ -1,21 +1,9 @@
-# $Id: $
+# $Id: ElectronID_cfi.py,v 1.1 2008/09/30 15:55:45 sixie Exp $
 
 import FWCore.ParameterSet.Config as cms
 
-from RecoEgamma.ElectronIdentification.electronIdCutBased_cfi import *
-from RecoEgamma.ElectronIdentification.electronIdCutBasedClasses_cfi import *
 from RecoEgamma.ElectronIdentification.electronIdLikelihood_cfi import *
-from RecoEgamma.ElectronIdentification.electronIdNeuralNet_cfi import *
-from RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi import *
-from RecoEgamma.ElectronIdentification.electronIdCutBasedClassesExt_cfi import *
 from RecoEgamma.ElectronIdentification.electronIdLikelihoodExt_cfi import *
-from RecoEgamma.ElectronIdentification.electronIdNeuralNetExt_cfi import *
 
-MitEIdSequence = cms.Sequence( eidCutBased +
-                            eidCutBasedExt +
-                            eidCutBasedClasses +
-                            eidCutBasedClassesExt +
-                            eidLikelihood +
-                            eidLikelihoodExt + 
-                            eidNeuralNet +
-                            eidNeuralNetExt)
+MitEIdSequence = cms.Sequence(eidLikelihood    +
+                              eidLikelihoodExt)
