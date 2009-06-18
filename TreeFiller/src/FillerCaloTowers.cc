9371e66f94fb42b7273a106e57d72f13c477eb84
bendavid <>
2009-06-18 23:06:37 +0000

    Updated for more flexible CaloTowers in 31X

--- TreeFiller/src/FillerCaloTowers.cc
+++ TreeFiller/src/FillerCaloTowers.cc
@@ -1,4 +1,4 @@
-// $Id: FillerCaloTowers.cc,v 1.10 2009/03/22 11:35:45 loizides Exp $
+// $Id: FillerCaloTowers.cc,v 1.11 2009/06/15 15:00:25 loizides Exp $
 
 #include "MitProd/TreeFiller/interface/FillerCaloTowers.h"
 #include "DataFormats/CaloTowers/interface/CaloTower.h"
@@ -76,28 +76,68 @@ void FillerCaloTowers::FillDataBlock(const edm::Event      &event,
     double deltaPos = (inCaloTower->hadPosition() - inCaloTower->emPosition()).mag();
     double deltaE = inCaloTower->energy() - inCaloTower->emEnergy() - inCaloTower->hadEnergy();
     
-    if ( mass > 1e-3)
-      throw edm::Exception(edm::errors::Configuration, "FillerCaloTowers::FillDataBlock()\n")
-         << "Error! reco::CaloTower mass = " << mass 
-         << ", not massless as assumed by mithep::CaloTower," << std::endl;
+//     if ( mass > 1e-3)
+//       throw edm::Exception(edm::errors::Configuration, "FillerCaloTowers::FillDataBlock()\n")
+//          << "Error! reco::CaloTower mass = " << mass 
+//          << ", not massless as assumed by mithep::CaloTower," << std::endl;
          
-    if ( deltaPos > 1e-3)
-      throw edm::Exception(edm::errors::Configuration, "FillerCaloTowers::FillDataBlock()\n")
-        << "Error! reco::CaloTower does not have identical HadPos and EmPos " 
-        << "as assumed by mithep::CaloTower, deltaPos = " << deltaPos << std::endl;
-         
+//     if ( deltaPos > 1e-3)
+//       throw edm::Exception(edm::errors::Configuration, "FillerCaloTowers::FillDataBlock()\n")
+//         << "Error! reco::CaloTower does not have identical HadPos and EmPos " 
+//         << "as assumed by mithep::CaloTower, deltaPos = " << deltaPos << std::endl;
+//          
     if ( deltaE > 1e-3)
       throw edm::Exception(edm::errors::Configuration, "FillerCaloTowers::FillDataBlock()\n")
         << "Error! reco::CaloTower default energy does not exclude HO " 
         << "as assumed by mithep::CaloTower, deltaE = " << deltaE << std::endl;
     
-    outCaloTower->SetPosition(inCaloTower->emPosition().x(),
-                                inCaloTower->emPosition().y(),
-                                inCaloTower->emPosition().z());
+//     outCaloTower->SetEMPosition(inCaloTower->emPosition().x(),
+//                                 inCaloTower->emPosition().y(),
+//                                 inCaloTower->emPosition().z());
+//     outCaloTower->SetHadPosition(inCaloTower->hadPosition().x(),
+//                                 inCaloTower->hadPosition().y(),
+//                                 inCaloTower->hadPosition().z());
+
+    ThreeVector avgPos;
+    ThreeVector emPos(inCaloTower->emPosition().x(),
+                      inCaloTower->emPosition().y(),
+                      inCaloTower->emPosition().z());
+    ThreeVector hadPos(inCaloTower->hadPosition().x(),
+                       inCaloTower->hadPosition().y(),
+                       inCaloTower->hadPosition().z());
+    double energyWeight = 0.0;
+    if (inCaloTower->emEnergy()>0.0) {
+      avgPos += inCaloTower->emEnergy()*emPos;
+      energyWeight += inCaloTower->emEnergy();
+    }
+    if (inCaloTower->hadEnergy()>0.0) {
+      avgPos += inCaloTower->hadEnergy()*hadPos;
+      energyWeight += inCaloTower->hadEnergy();
+    }
+    if (energyWeight > 0.0)
+      avgPos = (1.0/energyWeight)*avgPos;
+    else
+      avgPos = emPos;
+      
+    ThreeVectorC finalPos(avgPos.Rho(),inCaloTower->eta(),inCaloTower->phi());
+
+    outCaloTower->SetPosition(finalPos.x(),finalPos.y(),finalPos.z());
+
     outCaloTower->SetEmEnergy(inCaloTower->emEnergy());
     outCaloTower->SetHadEnergy(inCaloTower->hadEnergy());
     outCaloTower->SetOuterEnergy(inCaloTower->outerEnergy());
+    outCaloTower->SetMass(inCaloTower->mass());
     caloTowerMap_->Add(inCaloTower->id(),outCaloTower);
+    
+    if (verbose_>1) {
+      printf("reco::CaloTower:   pt=%5f, eta=%5f, phi=%5f, mass=%5f\n",inCaloTower->pt(),inCaloTower->eta(),inCaloTower->phi(),inCaloTower->mass());
+      printf("mithep::CaloTower: pt=%5f, eta=%5f, phi=%5f, mass=%5f\n", outCaloTower->Pt(),outCaloTower->Eta(),outCaloTower->Phi(),outCaloTower->Mass());
+      printf("emPos : rho=%5f, eta=%5f, phi=%5f\n",emPos.Rho(),emPos.Eta(),emPos.Phi());
+      printf("hadPos: rho=%5f, eta=%5f, phi=%5f\n",hadPos.Rho(),hadPos.Eta(),hadPos.Phi());
+      printf("avgPos: rho=%5f, eta=%5f, phi=%5f\n",avgPos.Rho(),avgPos.Eta(),avgPos.Phi());
+      printf("finPos: rho=%5f, eta=%5f, phi=%5f\n",finalPos.Rho(),finalPos.Eta(),finalPos.Phi());
+    }
+
   }
   caloTowers_->Trim();
 }
