c14f7e9a205715cc10d51688781622d248629a03
bendavid <>
2009-11-05 21:40:14 +0000

    Disable Pixel hits and tracks for AOD

--- BAMBUSequences/python/BambuFillAOD_cfi.py
+++ BAMBUSequences/python/BambuFillAOD_cfi.py
@@ -1,8 +1,18 @@
-# $Id: BambuFillAOD_cfi.py,v 1.1 2009/10/04 12:53:19 bendavid Exp $
+# $Id: BambuFillAOD_cfi.py,v 1.2 2009/10/19 18:44:51 bendavid Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 from MitProd.BAMBUSequences.BambuFillRECO_cfi import *
+
+#disable pixel hit/track producers/fillers
+try:
+  BambuRecoSequence.remove(siPixelRecHits)
+except:
+  pass
+  
+MitTreeFiller.PixelHits.active            = False
+MitTreeFiller.PixelVertexes.active        = False
+MitTreeFiller.PixelTracks.active          = False
 
 #Disable duplicate removal in StableParticle merging for conversions,
 #since this requires tracking RecHits which aren't available in AOD
