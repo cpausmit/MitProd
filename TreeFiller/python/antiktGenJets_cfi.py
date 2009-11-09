1bdc0fa86ff913d3ee98d55fd48094a59eb0a537
loizides <>
2009-11-09 07:46:36 +0000

    Not needed any longer

--- TreeFiller/python/antiktGenJets_cfi.py
+++
@@ -1,8 +0,0 @@
-# $Id: antiktGenJets_cfi.py,v 1.1 2009/07/22 04:41:00 bendavid Exp $
-
-import FWCore.ParameterSet.Config as cms
-
-from RecoJets.Configuration.GenJetParticles_cff import *
-from RecoJets.JetProducers.antikt7GenJets_cff import *
-
-antiktGenJets = cms.Sequence(genJetParticles*antikt7GenJets)
