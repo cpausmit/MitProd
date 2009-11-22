6637aedaeea082246a91cbdf051ea71b3f236c98
bendavid <>
2009-11-22 16:20:14 +0000

    Add special config file for startup MC

---
+++ Configuration/python/BAMBUProd_RECOSIMStartup.py
@@ -0,0 +1,50 @@
+# $Id:#
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('FILLER')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('Mit_011a'),
+    annotation = cms.untracked.string('RECOSIM'),
+    name = cms.untracked.string('BambuProduction')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.options = cms.untracked.PSet(
+   Rethrow = cms.untracked.vstring('ProductNotFound'),
+   fileMode =  cms.untracked.string('NOMERGE'),
+)
+
+# input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/build/bendavid/RECOSIMStartup/MinBiasStartup900GeV334/7A31312F-0AD7-DE11-9A93-0018F3D095F6.root')
+)
+process.source.inputCommands = cms.untracked.vstring("keep *","drop *_MEtoEDMConverter_*_*")
+
+# other statements
+
+#special global tag for 900GeV startup beamspot conditions and startup (CRAFT-knowledge) alignment scenario
+process.GlobalTag.globaltag = 'STARTUP3X_V8D::All'
+
+process.add_(cms.Service("ObjectService"))
+
+process.load("MitProd.BAMBUSequences.BambuFillRECOSIM_cfi")
+
+#hack pixelLess tracking back (present in special startup MC samples)
+process.MitTreeFiller.PixelLessTracks.active          = True
+
+process.bambu_step  = cms.Path(process.BambuFillRECOSIM)
+
+# schedule definition
+process.schedule = cms.Schedule(process.bambu_step)
