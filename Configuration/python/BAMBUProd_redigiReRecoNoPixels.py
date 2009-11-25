030f27ba87a36401e9fe10418d6d3752e3744b04
bendavid <>
2009-11-25 23:13:55 +0000

    Add special redigi-rereco with no pixels config

---
+++ Configuration/python/BAMBUProd_redigiReRecoNoPixels.py
@@ -0,0 +1,69 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.149 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: promptCollisionReco -s RAW2DIGI,L1Reco,RECO,DQM,ALCA:SiStripCalZeroBias --datatier RECO --eventcontent RECO --conditions CRAFT09_R_V4::All --scenario pp --no_exec --data --magField AutoFromDBCurrent -n 100
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+# import of standard configurations
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('FWCore/MessageService/MessageLogger_cfi')
+process.load('Configuration/StandardSequences/MixingNoPileUp_cff')
+process.load('Configuration/StandardSequences/GeometryIdeal_cff')
+process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/Digi_cff')
+process.load('Configuration/StandardSequences/SimL1Emulator_cff')
+process.load('Configuration/StandardSequences/DigiToRaw_cff')
+process.load('Configuration/StandardSequences/RawToDigi_cff')
+process.load('Configuration/StandardSequences/L1Reco_cff')
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load('Configuration/StandardSequences/EndOfProcess_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('Mit_012c'),
+    annotation = cms.untracked.string('redigiReRecoNoPixels'),
+    name = cms.untracked.string('BambuProduction')
+)
+#process.maxEvents = cms.untracked.PSet(
+#    input = cms.untracked.int32(10)
+#)
+process.options = cms.untracked.PSet(
+   Rethrow = cms.untracked.vstring('ProductNotFound'),
+   fileMode =  cms.untracked.string('NOMERGE'),
+)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file://GenHLT_8E29.root')
+)
+process.source.inputCommands = cms.untracked.vstring("keep *","drop *_MEtoEDMConverter_*_*")
+
+# Other statements
+process.GlobalTag.globaltag = 'STARTUP3X_V8D::All'
+
+#remove pixel detector at digi level
+process.DigiToRaw.remove(process.siPixelRawData)
+        
+process.add_(cms.Service("ObjectService"))
+
+process.load("MitProd.BAMBUSequences.BambuFillRECOSIM_cfi")
+
+process.MitTreeFiller.TreeWriter.fileName = 'XX-MITDATASET-XX'
+
+#hack pixelLess tracking back (present in special startup MC samples)
+process.MitTreeFiller.PixelLessTracks.active          = True
+
+# Path and EndPath definitions
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction_withPixellessTk)
+process.bambu_step  = cms.Path(process.BambuFillRECOSIM)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.bambu_step)
