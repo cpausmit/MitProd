a3b67572e50c9be122c1924d33a8873424d5c57f
bendavid <>
2010-01-07 11:05:06 +0000

    enable tracking particle filling for reco debug sequence

--- BAMBUSequences/python/BambuFillRECODEBUG_cfi.py
+++ BAMBUSequences/python/BambuFillRECODEBUG_cfi.py
@@ -1,4 +1,4 @@
-# $Id:$
+# $Id: BambuFillRECODEBUG_cfi.py,v 1.2 2009/07/22 11:26:19 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -8,5 +8,6 @@ from MitProd.TreeFiller.MitPostRecoGenerator_cff import *
 
 # Enable TrackingParticle association to MCParticles
 MitTreeFiller.MCParticles.trackingActive = True
+MitTreeFiller.MCParticles.fillTracking = True
 
 BambuFillRECODEBUG = cms.Sequence(mit_postreco_generator*BambuFillRECOSIM)
