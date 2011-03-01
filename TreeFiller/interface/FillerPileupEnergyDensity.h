ffb9566218e85e47dd50d37178d9868ae634b0f3
mzanetti <>
2011-03-01 14:26:30 +0000

    filler for fastjet computed pu energy density. First version

---
+++ TreeFiller/interface/FillerPileupEnergyDensity.h
@@ -0,0 +1,35 @@
+//--------------------------------------------------------------------------------------------------
+// $Id: FillerPileupEnergyDensity.h,v 1.1 2011/02/08 14:54:12 mzanetti Exp $
+//
+// FillerMet
+//
+// Implementation of a filler to fill Pileup Energy Density
+//
+// Authors: C.Loizides
+//--------------------------------------------------------------------------------------------------
+
+#ifndef MITPROD_TREEFILLER_FILLERPILEUPENERGYDENSITY_H
+#define MITPROD_TREEFILLER_FILLERPILEUPENERGYDENSITY_H
+
+#include "MitAna/DataTree/interface/PileupEnergyDensityFwd.h"
+#include "MitProd/TreeFiller/interface/AssociationMaps.h"
+#include "MitProd/TreeFiller/interface/BaseFiller.h"
+
+namespace mithep 
+{
+  class FillerPileupEnergyDensity : public BaseFiller
+  {  
+    public:
+      FillerPileupEnergyDensity(const edm::ParameterSet &cfg, const char *name, bool active=1);
+      ~FillerPileupEnergyDensity();
+
+      void                  BookDataBlock(TreeWriter &tws);
+      void 	            FillDataBlock(const edm::Event &e, const edm::EventSetup &es);
+  
+    private:
+      std::string                 edmName_;    //edm name of met collection
+      std::string                 mitName_;    //mit name of PileupEnergyDensitys
+      mithep::PileupEnergyDensityArr       *rhos_;       //array of PileupEnergyDensitys
+  };
+}
+#endif
---
+++ TreeFiller/src/FillerPileupEnergyDensity.cc
@@ -0,0 +1,56 @@
+// $Id: FillerPileupEnergyDensity.cc,v 1.1 2011/02/08 14:54:13 mzanetti Exp $
+
+#include "MitProd/TreeFiller/interface/FillerPileupEnergyDensity.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "MitAna/DataTree/interface/Names.h"
+#include "MitAna/DataTree/interface/PileupEnergyDensityCol.h"
+#include "MitProd/ObjectService/interface/ObjectService.h"
+
+using namespace std;
+using namespace edm;
+using namespace mithep;
+
+//--------------------------------------------------------------------------------------------------
+FillerPileupEnergyDensity::FillerPileupEnergyDensity(const ParameterSet &cfg, const char *name, bool active) : 
+  BaseFiller(cfg,name,active),
+  edmName_(Conf().getUntrackedParameter<string>("edmName","kt6PFJets")),
+  mitName_(Conf().getUntrackedParameter<string>("mitName",Names::gkPileupEnergyDensityBrn)),
+  rhos_(new mithep::PileupEnergyDensityArr)
+{
+  // Constructor.
+}
+
+//--------------------------------------------------------------------------------------------------
+FillerPileupEnergyDensity::~FillerPileupEnergyDensity()
+{
+  // Destructor.
+
+  delete rhos_;
+}
+
+//--------------------------------------------------------------------------------------------------
+void FillerPileupEnergyDensity::BookDataBlock(TreeWriter &tws)
+{
+  // Add pileup branch to tree.
+  tws.AddBranch(mitName_,&rhos_);
+  OS()->add<mithep::PileupEnergyDensityArr>(rhos_,mitName_);
+}
+
+//--------------------------------------------------------------------------------------------------
+void FillerPileupEnergyDensity::FillDataBlock(const edm::Event      &event, 
+					      const edm::EventSetup &setup)
+{
+  // Fill missing energy from edm collection into our collection.
+
+  rhos_->Delete();
+
+  Handle<double> hRho;
+  GetProduct(edmName_, hRho, event);
+
+  mithep::PileupEnergyDensity *rho = rhos_->AddNew();
+
+  rho->SetRho(*hRho);
+
+  rhos_->Trim();
+}
