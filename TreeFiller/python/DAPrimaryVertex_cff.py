0ff6a742191836c8a71a00d84b17f12bfbd5d758
mzanetti <>
2011-03-02 10:26:31 +0000

    cff for DAPrimaryVertex

---
+++ TreeFiller/python/DAPrimaryVertex_cff.py
@@ -0,0 +1,35 @@
+# $Id: FastJetCorrection_cff.py,v 1.1 2011/02/04 16:31:53 mzanetti Exp $
+
+import FWCore.ParameterSet.Config as cms
+
+offlinePrimaryVerticesDA = cms.EDProducer("PrimaryVertexProducer",
+    verbose = cms.untracked.bool(False),
+    algorithm = cms.string('AdaptiveVertexFitter'),
+    TrackLabel = cms.InputTag("generalTracks"),
+    useBeamConstraint = cms.bool(False),
+    beamSpotLabel = cms.InputTag("offlineBeamSpot"),
+    minNdof  = cms.double(0.0),
+    PVSelParameters = cms.PSet(
+        maxDistanceToBeam = cms.double(1.0)
+    ),
+    TkFilterParameters = cms.PSet(
+        algorithm=cms.string('filter'),
+        maxNormalizedChi2 = cms.double(5.0),
+        minPixelLayersWithHits=cms.int32(2),
+        minSiliconLayersWithHits = cms.int32(5),
+        maxD0Significance = cms.double(5.0), 
+        minPt = cms.double(0.0),
+        trackQuality = cms.string("any")
+    ),
+
+    TkClusParameters = cms.PSet(
+        algorithm   = cms.string("DA"),
+        TkDAClusParameters = cms.PSet( 
+            coolingFactor = cms.double(0.8),  #  rather slow annealing for now
+            Tmin = cms.double(9.),            #  end of annealing
+            vertexSize = cms.double(0.01)     #  100 um !!
+        )
+    )
+)
+
+daPrimaryVertexSequence = cms.Sequence( offlinePrimaryVerticesDA )
