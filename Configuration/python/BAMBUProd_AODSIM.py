fc65e5fba39e287bdef9f58ae1fb4328eed17b26
bendavid <>
2011-10-10 20:57:59 +0000

    top level configs examples

--- Configuration/python/BAMBUProd_AODSIM.py
+++ Configuration/python/BAMBUProd_AODSIM.py
@@ -1,4 +1,4 @@
-# $Id: BAMBUProd_AODSIM.py,v 1.30 2011/09/28 16:50:06 bendavid Exp $
+# $Id: BAMBUProd_AODSIM.py,v 1.31 2011/10/09 14:15:05 bendavid Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -34,8 +34,9 @@ process.source = cms.Source("PoolSource",
    #fileNames = cms.untracked.vstring('file:/data/blue/bendavid/423aodsim/884FBED5-AC91-E011-B64A-90E6BA442EEB.root'),
    #fileNames = cms.untracked.vstring('/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/RecoTest_PU_S5_START44_V4-v1/0000/1E2427EB-DACF-E011-A967-0026189438BA.root'),
    #fileNames = cms.untracked.vstring('/store/relval/CMSSW_4_4_0/RelValProdTTbar/AODSIM/START44_V5-v2/0046/74C825F5-ACE6-E011-8C0D-00261894391F.root'),
-   fileNames = cms.untracked.vstring('/store/relval/CMSSW_4_4_0/RelValProdTTbar/AODSIM/START44_V5-v2/0046/74C825F5-ACE6-E011-8C0D-00261894391F.root'),
-#   skipEvents = cms.untracked.uint32(861),
+   #fileNames = cms.untracked.vstring('/store/relval/CMSSW_4_4_0/RelValProdTTbar/AODSIM/START44_V5-v2/0046/74C825F5-ACE6-E011-8C0D-00261894391F.root'),
+   fileNames = cms.untracked.vstring('file:/data/blue/bendavid/fall11test/AE775E4D-1DE8-E011-9EB7-E0CB4E553665.root'),
+   #skipEvents = cms.untracked.uint32(1740),
 )
 process.source.inputCommands = cms.untracked.vstring("keep *", "drop *_MEtoEDMConverter_*_*", "drop L1GlobalTriggerObjectMapRecord_hltL1GtObjectMap__HLT")
 
