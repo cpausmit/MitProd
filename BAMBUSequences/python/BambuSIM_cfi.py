a8bb6f98b0703a7141667c2453f2d3e349843e23
pharris <>
2012-01-15 23:11:34 +0000

    *** empty log message ***

--- BAMBUSequences/python/BambuSIM_cfi.py
+++ BAMBUSequences/python/BambuSIM_cfi.py
@@ -1,4 +1,4 @@
-# $Id: BambuSIM_cfi.py,v 1.16 2010/11/25 21:30:12 bendavid Exp $
+# $Id: BambuSIM_cfi.py,v 1.17 2011/02/08 14:56:08 mzanetti Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -8,18 +8,18 @@ from MitProd.BAMBUSequences.BambuFillRECO_cfi import *
 from MitProd.TreeFiller.antiktGenJets_cfi import *
 
 # Load flavor history
-from PhysicsTools.HepMCCandAlgos.flavorHistoryProducer_cfi import *
-from PhysicsTools.HepMCCandAlgos.flavorHistoryFilter_cfi import *
+#from PhysicsTools.HepMCCandAlgos.flavorHistoryProducer_cfi import *
+#from PhysicsTools.HepMCCandAlgos.flavorHistoryFilter_cfi import *
 
 # Load Flavor Matching Information
-from MitProd.TreeFiller.JetsMCFlavourMatching_cfi import *
+#from MitProd.TreeFiller.JetsMCFlavourMatching_cfi import *
 
 BambuGenSequence = cms.Sequence(
-    bFlavorHistoryProducer*
-    cFlavorHistoryProducer*
-    flavorHistoryFilter*
-    caloJetMCFlavour*
-    pfJetMCFlavour
+#    bFlavorHistoryProducer*
+#    cFlavorHistoryProducer*
+#    flavorHistoryFilter*
+#    caloJetMCFlavour*
+#    pfJetMCFlavour
 )
 
 #function to enable sim filling for the given tree filler
@@ -41,33 +41,33 @@ def enableSIM(filler):
   filler.MCEventInfo.active = True
   filler.MCVertexes.active  = True
 #  filler.IC5GenJets.active  = True
-  filler.KT4GenJets.active  = True
-  filler.KT6GenJets.active  = True
-  filler.GenMet.active      = True
+#  filler.KT4GenJets.active  = True
+#  filler.KT6GenJets.active  = True
+#  filler.GenMet.active      = True
   
   #Enable filling antikt gen jets
-  filler.AKT5GenJets.active = True
-  filler.AKT7GenJets.active = True
+#  filler.AKT5GenJets.active = True
+#  filler.AKT7GenJets.active = True
   
   # Enable Flavor history filling
-  filler.MCEventInfo.flavorHistoryActive = True
+  #filler.MCEventInfo.flavorHistoryActive = True
 
   # Enable the pileup information
   filler.PileupInfo.active = True
   
   # Enable Flavor matching for Reco Jets and GenJets
   #filler.ItrCone5Jets.flavorMatchingActive   = True
-  filler.Kt4Jets.flavorMatchingActive        = True
-  filler.Kt6Jets.flavorMatchingActive        = True
-  filler.AKt5Jets.flavorMatchingActive       = True
-  filler.AKt7Jets.flavorMatchingActive       = True
+  #filler.Kt4Jets.flavorMatchingActive        = True
+  #filler.Kt6Jets.flavorMatchingActive        = True
+  #filler.AKt5Jets.flavorMatchingActive       = True
+  #filler.AKt7Jets.flavorMatchingActive       = True
 # filler.ItrCone5PFJets.flavorMatchingActive = True
-  filler.Kt4PFJets.flavorMatchingActive      = True
-  filler.Kt6PFJets.flavorMatchingActive      = True
-  filler.AKt5PFJets.flavorMatchingActive     = True
-  filler.AKt7PFJets.flavorMatchingActive     = True
+  #filler.Kt4PFJets.flavorMatchingActive      = True
+  #filler.Kt6PFJets.flavorMatchingActive      = True
+  #filler.AKt5PFJets.flavorMatchingActive     = True
+  #filler.AKt7PFJets.flavorMatchingActive     = True
 # filler.IC5GenJets.flavorMatchingActive     = True
-  filler.KT4GenJets.flavorMatchingActive     = True
-  filler.KT6GenJets.flavorMatchingActive     = True
-  filler.AKT5GenJets.flavorMatchingActive    = True
-  filler.AKT7GenJets.flavorMatchingActive    = True
+  #filler.KT4GenJets.flavorMatchingActive     = True
+  #filler.KT6GenJets.flavorMatchingActive     = True
+  #filler.AKT5GenJets.flavorMatchingActive    = True
+  #filler.AKT7GenJets.flavorMatchingActive    = True
