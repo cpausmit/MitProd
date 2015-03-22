a2fe7778667339f999c73cfdc892ca643f3e3bfa
Yutaro Iiyama <yiiyama@mit.edu>
2015-03-22 19:27:13 -0400

    removing __init__.py

--- BAMBUSequences42/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/MitProd/BAMBUSequences42/',1)[0])+'/cfipython/slc5_amd64_gcc462/MitProd/BAMBUSequences42')
