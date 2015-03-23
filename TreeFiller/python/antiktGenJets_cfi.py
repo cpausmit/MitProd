0c1067e5811b1419013b8c1ec59b01833ab599e0
Yutaro Iiyama <yiiyama@mit.edu>
2015-03-23 17:52:05 -0400

    at least remove python errors

--- TreeFiller/python/antiktGenJets_cfi.py
+++ TreeFiller/python/antiktGenJets_cfi.py
@@ -5,4 +5,4 @@ import FWCore.ParameterSet.Config as cms
 from RecoJets.Configuration.GenJetParticles_cff import *
 from RecoJets.Configuration.RecoGenJets_cff import *
 
-antiktGenJets = cms.Sequence(genJetParticles*ak5GenJets*ak7GenJets)
+antiktGenJets = cms.Sequence(genJetParticles*ak5GenJets*ak8GenJets)
