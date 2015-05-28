b61c8eb2aca91c79c335a21d71d8cadc109b9867
Yutaro Iiyama <yiiyama@mit.edu>
2015-05-28 14:52:28 -0400

    Adding PAT option to GenMet

--- TreeFiller/src/FillerGenMet.cc
+++ TreeFiller/src/FillerGenMet.cc
@@ -1,60 +1,72 @@
 #include "MitProd/TreeFiller/interface/FillerGenMet.h"
 #include "DataFormats/METReco/interface/GenMET.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
 #include "MitAna/DataTree/interface/GenMetCol.h"
 #include "MitAna/DataTree/interface/Names.h"
 #include "MitProd/ObjectService/interface/ObjectService.h"
 
-using namespace std;
-using namespace edm;
-using namespace mithep;
-
-//--------------------------------------------------------------------------------------------------
-FillerGenMet::FillerGenMet(const ParameterSet &cfg, edm::ConsumesCollector& collector, ObjectService* os, const char *name, bool active) : 
+mithep::FillerGenMet::FillerGenMet(edm::ParameterSet const& cfg, edm::ConsumesCollector& collector, mithep::ObjectService* os, char const* name, bool active/* = true*/) : 
   BaseFiller(cfg,os,name,active),
-  edmToken_(GetToken<reco::GenMETCollection>(collector, "edmName","genMetTrue")),
-  mitName_(Conf().getUntrackedParameter<string>("mitName",Names::gkGenMetBrn)),
+  edmToken_(GetToken<METView>(collector, "edmName", "genMetTrue")),
+  mitName_(Conf().getUntrackedParameter<std::string>("mitName", mithep::Names::gkGenMetBrn)),
+  fillFromPAT_(Conf().getUntrackedParameter<bool>("fillFromPAT", false)),
   genMets_(new mithep::GenMetArr)
 {
-  // Constructor.
 }
 
-//--------------------------------------------------------------------------------------------------
-FillerGenMet::~FillerGenMet()
+mithep::FillerGenMet::~FillerGenMet()
 {
-  // Destructor.
-
   delete genMets_;
 }
 
-//--------------------------------------------------------------------------------------------------
-void FillerGenMet::BookDataBlock(TreeWriter &tws)
+void
+mithep::FillerGenMet::BookDataBlock(mithep::TreeWriter& tws)
 {
-  // Add mets branch to tree.
-
-  tws.AddBranch(mitName_,&genMets_);
-  OS()->add<mithep::GenMetArr>(genMets_,mitName_);
+  tws.AddBranch(mitName_, &genMets_);
+  OS()->add(genMets_, mitName_);
 }
 
-//--------------------------------------------------------------------------------------------------
-void FillerGenMet::FillDataBlock(const edm::Event      &event, 
-                                  const edm::EventSetup &setup)
+void
+mithep::FillerGenMet::FillDataBlock(edm::Event const& event, 
+                                    edm::EventSetup const&)
 {
   // Fill missing energy from edm collection into our collection.
 
   genMets_->Delete();
 
-  Handle<reco::GenMETCollection> hGenMetProduct;
-  GetProduct(edmToken_, hGenMetProduct, event);
+  edm::Handle<METView> hMetView;
+  GetProduct(edmToken_, hMetView, event);
+  auto& metView = *hMetView;
 
-  reco::GenMETCollection const& inGenMets = *hGenMetProduct;
+  if (metView.size() != 0) {
+    if(fillFromPAT_) { 
+      pat::MET const* patMet = dynamic_cast<pat::MET const*>(&metView.at(0));
+      if (!patMet)
+        throw edm::Exception(edm::errors::Configuration, "FillerGenMet::FillDataBlock")
+          << "fillFromPAT set for non-PAT MET input";
+    }
+    else {
+      reco::GenMET const* genMet = dynamic_cast<reco::GenMET const*>(&metView.at(0));
+      if (!genMet)
+        throw edm::Exception(edm::errors::Configuration, "FillerGenMet::FillDataBlock")
+          << "Input is not a GenMETCollection";
+    }
+  }
 
-  // loop through all mets
-  for (reco::GenMETCollection::const_iterator inGenMet = inGenMets.begin(); 
-       inGenMet != inGenMets.end(); ++inGenMet) {
-    
+  for (auto& inMet : metView) {
+    reco::GenMET const* inGenMet = 0;
+
+    if (fillFromPAT_)
+      inGenMet = static_cast<pat::MET const&>(inMet).genMET();
+    else
+      inGenMet = static_cast<reco::GenMET const*>(&inMet);
+
+    if (!inGenMet)
+      continue;
+
     mithep::GenMet *genMet = genMets_->Allocate();
     new (genMet) mithep::GenMet(inGenMet->px(), inGenMet->py());
-    
+
     // fill Met base class data 
     genMet->SetSumEt(inGenMet->sumEt());
     genMet->SetElongitudinal(inGenMet->e_longitudinal());
