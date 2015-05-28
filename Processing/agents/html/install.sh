5cc4eca636ba04f545bf6e9e9acf4b4fd8af8c5c
Christoph Paus <paus@mit.edu>
2015-05-28 12:07:22 -0400

    Fix smaller permission issues and add link.

--- Processing/agents/html/install.sh
+++ Processing/agents/html/install.sh
@@ -19,15 +19,16 @@ cp $MIT_PROD_DIR/agents/html/agent*jpg $MIT_PROD_AGENTS_WWW
 # index files to log area
 echo " Generate index files"
 cp $MIT_PROD_DIR/agents/html/index.html $MIT_PROD_AGENTS_LOG
+ 
 
 cat $MIT_PROD_DIR/agents/html/index.php-Template \
-   | sed 's/XX-NAME-XX/reviewd/' | sed 's/XX-AKA-XX/Smith/' \
+   | sed 's/XX-NAME-XX/reviewd/g' | sed 's/XX-AKA-XX/Smith/' \
    > $MIT_PROD_AGENTS_LOG/reviewd/index.php
 cat $MIT_PROD_DIR/agents/html/index.php-Template \
-   | sed 's/XX-NAME-XX/catalogd/' | sed 's/XX-AKA-XX/Johnson/' \
+   | sed 's/XX-NAME-XX/catalogd/g' | sed 's/XX-AKA-XX/Johnson/' \
    > $MIT_PROD_AGENTS_LOG/catalogd/index.php
 cat $MIT_PROD_DIR/agents/html/index.php-Template \
-   | sed 's/XX-NAME-XX/cleanupd/' | sed 's/XX-AKA-XX/Williams/' \
+   | sed 's/XX-NAME-XX/cleanupd/g' | sed 's/XX-AKA-XX/Williams/' \
    > $MIT_PROD_AGENTS_LOG/cleanupd/index.php
 
 # update web pages from log area
@@ -35,5 +36,6 @@ echo " Sync files to the web area - no deletions"
 mkdir -p $MIT_PROD_AGENTS_WWW 
 rsync -Cavz $MIT_PROD_AGENTS_LOG $MIT_PROD_AGENTS_WWW/../
 
+chown ${MIT_PROD_USER}:${MIT_PROD_GROUP} -R $MIT_PROD_AGENTS_LOG $MIT_PROD_AGENTS_WWW
 
 exit 0
